ad3c871703e83e0e101fb917ce3a66e51505d0c3
yceffort <yceffort@gmail.com>
2026-05-30 17:23:10 +0900

    ♻️ Extract getContactHref to shared, drop dead date util

--- packages/shared/src/utils/index.ts
+++ packages/shared/src/utils/index.ts
@@ -1,4 +1,4 @@
-export {formatDate, formatDateLong, formatDateShort} from './date'
 export {getCookie, setCookie} from './cookie'
 export {stripTitleEmphasis, parseTitleEmphasis} from './title'
 export type {TitlePart} from './title'
+export {getContactHref} from './contact'
